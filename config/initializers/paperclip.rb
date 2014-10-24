7933af680ab04c2a5ae07f012d563542ef141bc5
Jusabe Samuel <jusabe.09@gmail.com>
2014-10-23 21:51:51 -0300

    aws domain

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = "/uploads/:class/:attachment/:id/:style/:filename"
+Paperclip::Attachment.default_options[:s3_host_name] = 'whatagenius.s3.amazonaws.com'
